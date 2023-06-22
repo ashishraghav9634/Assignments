ee43b0e4358f1ca501bb301fb8175aa171d72eea
Ashish Raghav <136688194+ashishraghav9634@users.noreply.github.com>
2023-06-22 21:13:06 +0530

    Add files via upload

---
+++ ASHISHJKG.java
@@ -0,0 +1,10 @@
+package studyopedia;
+
+public class ASHISHJKG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("HELLO ASHISH KUMAr");
+	}
+
+}

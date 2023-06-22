bebf066911844417fc533eaf1d04223e4402bb34
Ashish Raghav <136688194+ashishraghav9634@users.noreply.github.com>
2023-06-22 13:25:49 +0530

    Add files via upload

---
+++ HeLLOASHISH.java
@@ -0,0 +1,10 @@
+package studyopedia;
+
+public class HeLLOASHISH {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Hello AShish");
+	}
+
+}
